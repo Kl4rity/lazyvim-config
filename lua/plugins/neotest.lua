c50de95df8aa58afc06a91635ef509895deff59d
Clemens Stift <clemens@stift.online>
2025-11-17 12:18:59 +0100

    Initial commit

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-neotest/neotest",
+  opts = {
+    adapters = {
+      -- Jest adapter for JavaScript/TypeScript projects
+      ["neotest-jest"] = {
+        jestCommand = "npm test --",
+        jestConfigFile = "jest.config.js",
+        env = { CI = true },
+        cwd = function()
+          return vim.fn.getcwd()
+        end,
+      }, -- In your neotest configuration
+
+      ["neotest-gradle"] = {
+        -- This adapter uses Gradle's built-in test reporting
+      },
+    },
+  },
+  dependencies = {
+    "haydenmeade/neotest-jest",
+    "weilbith/neotest-gradle",
+  },
+}
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/deliberate/.config/omarchy/current/theme/neovim.lua
