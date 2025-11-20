31363a7b884219f8dcc2f432e79ef42efce48878
Clemens Stift <clemens@stift.online>
2025-11-20 18:27:44 +0100

    Install kotlin-lsp

--- lua/plugins/languages/kotlin.lua
+++ lua/plugins/languages/kotlin.lua
@@ -20,7 +20,37 @@ vim.schedule(function()
   }
 end)
 
+-- Kotlin LSP setup
+vim.schedule(function()
+  local lspconfig = require("lspconfig")
+  lspconfig.configs.kotlin_lsp = {
+    default_config = {
+      cmd = { "java", "-jar", vim.fn.expand("~/.local/share/nvim/mason/packages/kotlin-lsp/kotlin-lsp.jar") },
+      filetypes = { "kotlin" },
+      root_dir = lspconfig.util.root_pattern(
+        "settings.gradle.kts",
+        "settings.gradle",
+        "build.gradle.kts",
+        "build.gradle"
+      ),
+      settings = {},
+    },
+  }
+  lspconfig.kotlin_lsp.setup({
+    cmd_env = {
+      JAVA_HOME = "/home/deliberate/.local/share/mise/installs/java/17.0.2",
+    },
+  })
+end)
+
 return {
+  -- Kotlin mason packages
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = { "kotlin-lsp" },
+    },
+  },
   -- Kotlin testing configuration
   {
     "nvim-neotest/neotest",
